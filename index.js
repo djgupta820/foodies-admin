1496df3943d611ed4edecee1bcb38b897d9ee24c
Dheeraj Gupta <djgupta820@gmail.com>
2023-06-14 22:08:49 +0530

    index routes changed to user routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const ejsMate = require('ejs-mate')
+const userRoutes = require('./routes/userRoots')
 const app = express()
 const port = 3000
 
@@ -10,13 +11,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
-app.get('/login', (req, res)=>{
-    res.render('login')
-})
-
-app.get('/register', (req, res)=>{
-    res.render('register')
-})
+app.use('/user', userRoutes)
 
 app.get('/', (req, res)=>{
     res.render('index')
