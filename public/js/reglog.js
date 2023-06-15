c01ea52e1c23c5a568b34a1e5005af2a6d1e6b9c
Dheeraj Gupta <djgupta820@gmail.com>
2023-06-15 20:38:59 +0530

    added geocoding api to get user's current address

--- public/js/reglog.js
+++ public/js/reglog.js
@@ -1,36 +1,61 @@
 const login = document.querySelector('#login')
 const register = document.querySelector('#register')
+const loc = document.querySelector('#loc')
 
-function createMessage(data, parentElemet, messageType){
+function createMessage(data, parentElemet, messageType) {
     const div = document.createElement('div')
     div.className = 'message-' + messageType
     const text = document.createTextNode(data)
     div.append(text)
     parentElemet.parentNode.insertBefore(div, parentElemet)
-    setTimeout(()=>{
+    setTimeout(() => {
         parentElemet.parentNode.removeChild(div)
     }, 3000)
 }
 
-if(login){
-    login.addEventListener('submit', (e)=>{
+async function getCurrentLocation() {
+    const api = 'e7ea121f1d534eba9edb15b181c253c1'
+    let lat = ''
+    let lng = ''
+
+    // getting longitude and latitude
+    navigator.geolocation.getCurrentPosition((data)=>{
+        lat = data.coords.latitude
+        lng = data.coords.longitude
+        const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=${api}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            document.querySelector('#address').value = data.results[0].formatted
+        })
+    })
+}
+
+if (login) {
+    login.addEventListener('submit', (e) => {
         e.preventDefault()
         const username = login['username'].value
         const password = login['password'].value
-        if(username === ''){
+        if (username === '') {
             createMessage("Please input username!", login, 'danger')
         }
-        else if(password === ''){
+        else if (password === '') {
             createMessage("Please input password!", login, 'danger')
         }
-        else{
+        else {
             login.submit()
         }
     })
 }
 
-if(register){
-    register.addEventListener('submit', (e)=>{
+if(loc){
+    loc.addEventListener('click', (e)=>{
+        getCurrentLocation()
+    })
+}
+
+if (register) {
+    register.addEventListener('submit', (e) => {
         e.preventDefault()
         const pattern = /[^0-9]/
         const name = register['name'].value
@@ -38,25 +63,25 @@ if(register){
         const phone = register['phone'].value
         const address = register['address'].value
         const password = register['password'].value
-        if(name.length <= 3){
+        if (name.length <= 3) {
             createMessage("Name must have more than 3 characters!", register, 'danger')
         }
-        else if(email.length <= 0){
+        else if (email.length <= 0) {
             createMessage("Please input your email!", register, 'danger')
         }
-        else if(phone.match(pattern)){
+        else if (phone.match(pattern)) {
             createMessage("Phone number cannot contain alphabetical characters!", register, 'danger')
         }
-        else if(phone.length < 10 || phone.length > 10){
+        else if (phone.length < 10 || phone.length > 10) {
             createMessage("Phone number must be 10 digits long!", register, 'danger')
         }
-        else if(address.length <= 0){
+        else if (address.length <= 0) {
             createMessage("Please input your address!", register, 'danger')
         }
-        else if(password.length < 8 || password.length > 15){
+        else if (password.length < 8 || password.length > 15) {
             createMessage("Password length must be between 8 and 15 characters!", register, 'danger')
         }
-        else{
+        else {
             register.submit()
         }
     })
